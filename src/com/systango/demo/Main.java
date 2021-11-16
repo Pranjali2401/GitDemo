42c26506f0cfafca9b02fcc6e857841028cf4d97
Pranjali <pkhandelwal@isystango.com>
2021-11-16 10:50:57 +0530

    FEAT: add addition:

---
+++ src/com/systango/demo/Main.java
@@ -0,0 +1,12 @@
+package com.systango.demo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 12, b = 56;
+		System.out.println(a + b);
+
+	}
+
+}
